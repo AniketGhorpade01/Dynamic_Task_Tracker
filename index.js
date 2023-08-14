3061dc309eea4222205fc18c400b6a4cd4f7247b
AniketGhorpade01 <123549561+AniketGhorpade01@users.noreply.github.com>
2023-08-14 19:03:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 import _ from 'lodash';
 
 const app=express();
-const port=process.env.PORT || 3030
+const port=process.env.PORT || 3030;
 // const port=3000;
 // var todo=[];
 // var work=[];
